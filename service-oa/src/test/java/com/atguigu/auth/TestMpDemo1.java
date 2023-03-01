b87bc08ee55199861a152092b13fa51ac0fbb897
wang <wang-weijian@outlook.com>
2023-03-01 18:33:34 +0800

    3.1-4 测试结束

--- service-oa/src/test/java/com/atguigu/auth/TestMpDemo1.java
+++ service-oa/src/test/java/com/atguigu/auth/TestMpDemo1.java
@@ -20,4 +20,33 @@ public class TestMpDemo1 {
         List<SysRole> list = mapper.selectList(null);
         System.out.println(list);
     }
+
+    @Test
+    public void add() {
+        SysRole role = new SysRole();
+        SysRole sysRole = new SysRole();
+        sysRole.setRoleName("角色管理员1");
+        sysRole.setRoleCode("role");
+        sysRole.setDescription("角色管理员1");
+
+        int result = mapper.insert(sysRole);
+        System.out.println(result); //影响的行数
+        System.out.println(sysRole); //id自动回填
+    }
+
+    @Test
+    public void update() {
+        SysRole sysRole = new SysRole();
+        sysRole.setId(9L);
+        sysRole.setRoleName("角色管理员1");
+
+        int result = mapper.updateById(sysRole);
+        System.out.println(result);
+    }
+
+    @Test
+    public void deleteId() {
+        int rows = mapper.deleteById(10);
+    }
+
 }
