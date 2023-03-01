35d4b3508adc015cdf1abdc21e5dc2050039fbbf
wang <wang-weijian@outlook.com>
2023-03-01 16:01:47 +0800

    3.1-3 org改成com guigu改成wwj

---
+++ model/src/main/java/com/wwj/vo/system/SysOperLogQueryVo.java
@@ -0,0 +1,15 @@
+package com.wwj.vo.system;
+
+import lombok.Data;
+
+@Data
+public class SysOperLogQueryVo {
+
+	private String title;
+	private String operName;
+
+	private String createTimeBegin;
+	private String createTimeEnd;
+
+}
+
